8cc22a7a6389654edbf41065e30a3c2add976386
shu_ch@instance-1 <shuna966@gmail.com>
2020-01-16 17:53:29 +0900

    200117

---
+++ var-decname.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# @(#) hoge.sh ver.1.0.0 2008.04.24
+#
+# Usage:
+#   hoge.sh param1 param2
+#     param1 - パラメータ1です.
+#     param2 - パラメータ2です.
+#
+# Description:
+#   hoge.shスクリプトです.
+#
+###########################################################################
+
+for i in `seq 5`
+do
+	echo $i
+done
+
+
+
+
+echo -e "Enter to continue...\c"
+read dummy
+

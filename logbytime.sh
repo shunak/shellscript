fa951ccccd551c7b6a2de687b7e92990172c14b4
shu_ch@instance-1 <shuna966@gmail.com>
2020-01-22 17:57:30 +0900

    200121

---
+++ logbytime.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# @(#) hoge.sh ver.1.0.0 2008.04.24
+#
+# Usage:
+#   hoge.sh param1 param2
+#     param1 - パラメータ1です.
+#     param2 - パラメータ2です.
+#
+# Description:
+#   hoge.shスクリプトです.
+#
+###########################################################################
+#move to log dir
+LOGDIR='/var/log/'
+cd $LOGDIR
+#refer syslog arg $1
+LOGFILE='syslog'
+cat $LOGFILE | grep "^.\+ $1:.\+$"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
